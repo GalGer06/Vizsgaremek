0ec85b1baa1ebb27f33d3d990c38b0a33bb3f9fe
GalGer06 <galambos.gergo06@gmail.com>
2026-01-26 13:14:48 +0100

    create feladatok dto

--- backend/src/feladatok/dto/create-feladatok.dto.ts
+++ backend/src/feladatok/dto/create-feladatok.dto.ts
@@ -1,1 +1,27 @@
-export class CreateFeladatokDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateFeladatokDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  question: string;
+
+  @IsString()
+  @IsNotEmpty()
+  answer: string;
+
+  @IsString()
+  @IsNotEmpty()
+  correct: string;
+
+  @IsString()
+  @IsNotEmpty()
+  funfact: string;
+
+  @IsString()
+  @IsNotEmpty()
+  history: string;
+}
