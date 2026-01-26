e153ae2ea75729d7d660d061a4b801f6bb409b3f
GalGer06 <galambos.gergo06@gmail.com>
2026-01-26 13:19:31 +0100

    create userdata dto

--- backend/src/userdatas/dto/create-userdata.dto.ts
+++ backend/src/userdatas/dto/create-userdata.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserdataDto {}
+import { IsInt, IsOptional, Min } from "class-validator";
+
+export class CreateUserdataDto {
+  @IsInt()
+  userId: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  streak?: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  totalPoints?: number;
+
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  level?: number;
+}
