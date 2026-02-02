f2c1920408dffcfea7f08683e2653d67a125c403
GalGer06 <galambos.gergo06@gmail.com>
2026-02-02 12:03:06 +0100

    userdatas service

--- backend/src/userdatas/userdatas.module.ts
+++ backend/src/userdatas/userdatas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserdatasService } from './userdatas.service';
 import { UserdatasController } from './userdatas.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [UserdatasController],
-  providers: [UserdatasService],
+  providers: [UserdatasService, PrismaService],
 })
 export class UserdatasModule {}
--- backend/src/userdatas/userdatas.service.ts
+++ backend/src/userdatas/userdatas.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserdataDto } from './dto/create-userdata.dto';
 import { UpdateUserdataDto } from './dto/update-userdata.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserdatasService {
-  create(createUserdataDto: CreateUserdataDto) {
-    return 'This action adds a new userdata';
+  constructor (private prisma: PrismaService) {}
+
+  async create(createUserdataDto: CreateUserdataDto) {
+    return this.prisma.userDatas.create({
+      data: createUserdataDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all userdatas`;
+  async findAll() {
+    return this.prisma.userDatas.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} userdata`;
+  async findOne(id: number) {
+    return this.prisma.userDatas.findUnique({
+      where:{id}
+    });
   }
 
-  update(id: number, updateUserdataDto: UpdateUserdataDto) {
-    return `This action updates a #${id} userdata`;
+  async update(id: number, updateUserdataDto: UpdateUserdataDto) {
+    return this.prisma.userDatas.update({
+      where: { id },
+      data: updateUserdataDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} userdata`;
+  async remove(id: number) {
+    return this.prisma.userDatas.delete({
+      where: { id },
+    });
   }
 }
