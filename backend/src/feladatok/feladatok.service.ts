44ea0d4000006358aa0d499ae57a07f602733df0
GalGer06 <galambos.gergo06@gmail.com>
2026-02-02 13:08:30 +0100

    feladatok service javítás

--- backend/src/feladatok/feladatok.service.ts
+++ backend/src/feladatok/feladatok.service.ts
@@ -17,20 +17,20 @@ export class FeladatokService {
     return this.prisma.feladatok.findMany();
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return this.prisma.feladatok.findUnique({
       where: { id },
     });
   }
 
-  update(id: number, updateFeladatokDto: UpdateFeladatokDto) {
+  async update(id: number, updateFeladatokDto: UpdateFeladatokDto) {
     return this.prisma.feladatok.update({
       where: { id },
       data: updateFeladatokDto,
     });
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return this.prisma.feladatok.delete({
       where: { id },
     });
