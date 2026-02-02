653ba53e72b1fd012fa352e98cc8245e444c851a
GalGer06 <galambos.gergo06@gmail.com>
2026-02-02 11:19:37 +0100

    createw user dto

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsBoolean, IsDate, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  access: boolean;
+
+  @IsDate()
+  createdAt: Date;
+}
